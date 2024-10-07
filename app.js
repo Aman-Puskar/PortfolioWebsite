0148aa2318ce89c1dc8531b6c925161c6e6acb2e
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-07 12:27:42 +0530

    javascript

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function togglemenu() {
-    const menu = document.querySelector()
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".humburger-menu");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
 }
